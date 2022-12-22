0b924f7b818e9eae82ff32b57cb1405bdc98826c
Sabahey <sabahey9@gmail.com>
2022-12-22 05:57:09 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@
  * Return: A pointer to resulting string dest.
  */
 char *_strncpy(char *dest, char *src, int n)
-}
+{
 	int index = 0, src_len = 0;
 
 	while (src[index++])
@@ -18,6 +18,9 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
